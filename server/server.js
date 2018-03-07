fbb12d3e87f955189f20460ecf8865f73332c744
Ben Keating <ben.l.keating@gmail.com>
2018-03-07 22:10:48 +0000

    =added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Users} = require('./models/user.js');
 //3rd party modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -31,6 +32,26 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/12241453
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+  //validate ID using isvalid
+  if (!ObjectID.isValid(id)){
+    res.status(404).send('invalid ID input');  //respond with 404
+  } else {
+    //findById
+    Todo.findById(id).then((todo) => {
+      if(!todo){
+        //send back 404 if null
+        res.status(404).send('Todo not found')
+      }else {
+        //success case - send back
+        res.status(200).send({todo});
+      }
+    }).catch((e) => res.status(400).send()); //catch error case - 400
+  };
 });
 
 app.listen(3000, () => {
