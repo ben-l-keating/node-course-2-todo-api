37c88aa9f73aa042e335ac3d83a13b59e6dafde4
Ben Keating <ben.l.keating@gmail.com>
2018-03-12 22:24:14 +0000

    =add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,59 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UsersSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      minLength: 1,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email'
+      }
+    },
+    password: {
+      type: String,
+      require: true,
+      minLength: 6
+    },
+    tokens: [{
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required:true
+      }
+    }]
+});
+
+UsersSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UsersSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
 
 //create user model
 //store email - require and trim, set string and min length 1
-var Users = mongoose.model('Users', {
-  email: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true
-  }
-});
+var Users = mongoose.model('Users', UsersSchema);
 
 module.exports = {
   Users
