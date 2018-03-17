112765c16651c56f90b331feb59323a0a3da413d
Ben Keating <ben.l.keating@gmail.com>
2018-03-17 21:40:39 +0000

    =update conifg to use JSON file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users =[{
   password: 'userOnePass',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: user1Id, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: user1Id, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }, {
   _id: user2Id,
@@ -20,7 +20,7 @@ const users =[{
   password: 'userTwoPass',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: user2Id, access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: user2Id, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }];
 
